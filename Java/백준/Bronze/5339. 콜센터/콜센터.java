d707823e25bc2101a9286c81442033a74373fa67
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2025-01-16 23:26:44 +0900

    [Bronze V] Title: 콜센터, Time: 100 ms, Memory: 13892 KB -BaekjoonHub

---
+++ Java/백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        print();
+    }
+
+    static void print() {
+        System.out.print(
+        		"     /~\\\n"
+        		+ "    ( oo|\n"
+        		+ "    _\\=/_\n"
+        		+ "   /  _  \\\n"
+        		+ "  //|/.\\|\\\\\n"
+        		+ " ||  \\ /  ||\n"
+        		+ "============\n"
+        		+ "|          |\n"
+        		+ "|          |\n"
+        		+ "|          |"
+        );
+    }
+}

2c23ccbd9e6bf2938006bcfe0d23ca2330989c83
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2025-06-22 02:26:16 +0900

    [Silver II] Title: N과 M (9), Time: 268 ms, Memory: 37592 KB -BaekjoonHub

--- Java/백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ Java/백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,73 +1,75 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.StringTokenizer;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+	// 그냥 순열인거같은데 ? => 중복된 값은 하나만 출력해야된다.
+	static int N, M;
 	static int[] arr;
 	static int[] sel;
 	static boolean[] v;
-	static List<int[]> list = new ArrayList<int[]>();
-	
-	static void permutation(int idx, int k) {
-		if(sel.length == k) {
-			int[] copySel = Arrays.copyOf(sel, sel.length);
-			list.add(copySel);
-//			System.out.println("print" + Arrays.toString(sel));
-			return;
-		}
-		if(arr.length == idx) return;
-		
-		for (int i = 0; i < arr.length; i++) {
-			if(!v[i]) {
-				v[i] = true;
-				sel[idx] = arr[i];
-				permutation(idx+1, k+1);
-				v[i] = false;
-			}
-		}
-		
-	}
+	static List<List<Integer>> list;
+	static Set<String> set;
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		arr = new int[N];
-		sel = new int[M];
-		v = new boolean[N];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < arr.length; i++) {
+		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(arr);
-//		for(int a : arr)
-//			System.out.println(a + " ");
-		permutation(0, 0);
-		List<List<Integer>> tempList = new ArrayList<>();
-		for(int[] arr : list) {
-			tempList.add(Arrays.stream(arr).boxed().collect(Collectors.toList()));
-		}
-		Set<List<Integer>> set = new LinkedHashSet<List<Integer>>(tempList);
-		List<List<Integer>> newlist = new ArrayList<List<Integer>>(set);
-		
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < newlist.size(); i++) {
-			for (int j = 0; j < newlist.get(i).size(); j++) {
-				sb.append(newlist.get(i).get(j)+" ");
-			}
-			sb.append('\n');
-		}
+		sel = new int[M];
+		v = new boolean[N];
+		list = new ArrayList<>();
+		set = new LinkedHashSet<>();
+		permutation(0);
 		System.out.println(sb);
 	}
-
+	private static void permutation(int k) {
+		if(sel.length == k) {
+			// 배열 -> 리스트(중복을 비교할 땐, 리스트를 사용한다. 배열은 비교가 안된다.)
+//			List<Integer> tempList = new ArrayList<>();
+//			for(int val : sel) {
+//				tempList.add(val);
+//			}
+//			if(!list.contains(tempList)) {
+//				list.add(tempList);
+//				for(int val : sel) {
+//					sb.append(val).append(" ");
+//				}
+//				sb.append("\n");
+//			}
+//			근데 리스트로 비교하면 모든 리스트를 순회하면서 비교하니깐 시간 복잡도가 O(N!)*M 임
+//			그래서 비교할 때는 Set<String> 을 사용해야함
+			
+			// 배열 -> String
+			StringBuilder temp = new StringBuilder();
+			for(int val : sel) {
+				temp.append(val).append(" ");
+			}
+			String s = temp.toString();
+			
+			if(!set.contains(s)) {
+				set.add(s);
+				for(int val : sel) {
+					sb.append(val).append(" ");
+				}
+				sb.append("\n");
+			}
+			return;
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			if(!v[i]) {
+				v[i] = true;
+				sel[k] = arr[i];
+				permutation(k+1);
+				v[i] = false;
+			}
+		}
+	}
 }

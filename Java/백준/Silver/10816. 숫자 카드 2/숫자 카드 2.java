fec7957a10dfee30c297080e451165885fc3c893
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2025-07-22 01:03:46 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 996 ms, Memory: 183132 KB -BaekjoonHub

--- Java/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ Java/백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,57 +1,24 @@
-
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N, M;
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		N = Integer.parseInt(br.readLine());
-		int[] n_arr = new int[N];
+		Map<Integer, Integer> map = new HashMap<>();
+		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			n_arr[i] = Integer.parseInt(st.nextToken());
+			int key = Integer.parseInt(st.nextToken());
+			map.put(key, map.getOrDefault(key, 0) + 1);
 		}
-		Arrays.sort(n_arr);
 		
-		M = Integer.parseInt(br.readLine());
-		int[] m_arr = new int[M];
+		int M = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < M; i++) {
 			int target = Integer.parseInt(st.nextToken());
-			sb.append(right(n_arr, target) - left(n_arr, target) + " ");
+			sb.append(map.getOrDefault(target, 0) + " ");
 		}
 		System.out.println(sb);
-	}
-	static int left(int[] arr, int target) {
-		int left = 0;
-		int right = N;
-		while(left < right) {
-			int mid = (left+right)/2;
-			
-			if(arr[mid]>=target) {
-				right = mid;
-			}else {
-				left = mid+1;
-			}
-		}
-		return left;
-	}
-	
-	static int right(int[] arr, int target) {
-		int left = 0;
-		int right = N;
-		while(left < right) {
-			int mid = (left+right)/2;
-			
-			if(arr[mid]>target) {
-				right = mid;
-			}else {
-				left = mid+1;
-			}
-		}
-		return left;
-	}
+    }
 }
-

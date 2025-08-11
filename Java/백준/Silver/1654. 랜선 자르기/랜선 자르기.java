2119758613770805c268ec5f652f1c81766c027b
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2025-08-11 17:46:34 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 16992 KB -BaekjoonHub

--- Java/백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ Java/백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -4,46 +4,47 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int K, N;
-	static int[] arr;
+	static long K, N, max;
+	static long[] arr;
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		K = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
-		arr = new int[K];
+		K = Long.parseLong(st.nextToken());
+		N = Long.parseLong(st.nextToken());
+		arr = new long[(int) K];
+		max = Integer.MIN_VALUE;
 		for (int i = 0; i < K; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = Long.parseLong(br.readLine());
+			max = Math.max(max, arr[i]);
 		}
-		Arrays.sort(arr);
-//		System.out.println(Arrays.toString(arr));
-		long left = 0;
-		long right = arr[arr.length-1];
-		long mid = 0;
-		right++;
-//		System.out.println(right);
 		
-		// N개보다 많이 드는 것도 N개를 만드는 것에 포함
+		long result = bs();
+		System.out.println(result);
+	}
+	
+	private static long bs() {
+		long start=1;
+		long end=max;
 		
-		// 
-		while(right > left) {
-			mid = (left+right)/2;
-			long cnt = 0;
-			for (int i = 0; i < arr.length; i++) {
-				cnt+=(arr[i]/mid);
+		while(start<=end) {
+			long mid=(start+end)/2;
+//			System.out.println(mid);
+			long length=0;
+			for(long a : arr) {
+				length+=(a / mid);
 			}
-//			System.out.println(cnt);
-//			break;
-			if(cnt<N) {
-				right = mid;
+			
+//			System.out.println("length: " + length);
+			if(length<N) {
+				// N보다 길이가 크면 나누는 숫자를 높여야 한다.
+				end = mid-1;
 			}else {
-				left = mid+1;
+				// 작으면 나누는 숫자를 줄여야 한다.
+				start = mid+1;
 			}
-//			System.out.println(left + " " + right + " " + mid);
-//			System.out.println(cnt);
 		}
-		System.out.println(left-1);
+		
+		return start-1;
 	}
 }
-//1 2 3 10000

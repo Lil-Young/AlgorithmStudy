3623b794e4e66446bc999e57e4f2e6c878343896
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2025-09-02 16:26:18 +0900

    [Gold IV] Title: 인구 이동, Time: 476 ms, Memory: 46084 KB -BaekjoonHub

--- Java/백준/Gold/16234. 인구 이동/인구 이동.java
+++ Java/백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,16 +1,23 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, L, R;
-    static int[][] arr;
-    static boolean[][] v;
-    static int[] dr = {0, 0, 1, -1};
-    static int[] dc = {1, -1, 0, 0};
-    
-    static int sum, count;
-    static List<int[]> union;
-    
+	static int N, L, R;
+	static boolean isMove;
+	static int[][] arr;
+	static boolean[][] v;
+	static int[] dr = {0, 0, 1, -1};
+	static int[] dc = {1, -1, 0, 0};
+	static List<Point> list;
+	
+	static class Point{
+		int r, c;
+		Point(int r, int c){
+			this.r=r;
+			this.c=c;
+		}
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,59 +25,79 @@ public class Main {
 		L = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
 		arr = new int[N][N];
-		for(int i=0; i<N; i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
+			for (int j = 0; j < N; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		int day = 0;
+		int result = 0;
 		while(true) {
-			v = new boolean[N][N];
-			boolean moved = false;
+			isMove = false;
 			
+			v = new boolean[N][N];
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					if(v[i][j]) continue;
-					
-					union = new ArrayList<>();
-					sum=0;
-					count=0;
-					
-					dfs(i, j);
-					
-					if(union.size() >= 2) {
-						int avg = sum / count;
-						for(int[] a : union) {
-							arr[a[0]][a[1]] = avg;
+					if(!v[i][j]) {
+						boolean isPossible = check(i, j);
+						if(!isPossible) continue;
+						isMove = true;
+						list = new ArrayList<>();
+						// list에 공유하는 좌표를 넣는다.
+						bfs(i, j);
+						
+						// list에 있는 값을 전부 더해서 평균을 만든다.
+						int sum = 0;
+						for(Point p : list) {
+							sum+=arr[p.r][p.c];
 						}
-						moved = true;
+						int avg = sum/list.size();
+						// list에 있는 값을 평균으로 변경한다.
+						for(Point p : list) {
+							arr[p.r][p.c] = avg;
+						}
 					}
 				}
 			}
-			
-			if(!moved) {
-				break;
-			}
-			day++;
+			if(!isMove) break;
+			result++;
 		}
-		System.out.println(day);
-	}
-	private static void dfs(int r, int c) {
-		v[r][c] = true;
-		union.add(new int[] {r, c});
-		sum+=arr[r][c];
-		count++;
+		System.out.println(result);
 		
+	}
+	private static boolean check(int r, int c) {
+		// 먼저 사방에 L이상 R이하인 좌표가 있는지 확인한다.
 		for (int d = 0; d < 4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			if(nr<0 || nr>=N || nc<0 || nc>=N || v[nr][nc]) continue;
-			
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			if(nr<0 || nr>=N || nc<0 || nc>=N) continue;
 			int gap = Math.abs(arr[r][c] - arr[nr][nc]);
-			if(gap >= L && gap <= R) {
-				dfs(nr, nc);
+			if(gap>=L && gap<=R) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private static void bfs(int r, int c) {
+		Queue<Point> queue = new ArrayDeque<>();
+		queue.offer(new Point(r, c));
+		v[r][c] = true;
+		list.add(new Point(r, c));
+		
+		while(!queue.isEmpty()) {
+			Point p = queue.poll();
+			for (int d = 0; d < 4; d++) {
+				int nr = p.r + dr[d];
+				int nc = p.c + dc[d];
+				if(nr<0 || nr>=N || nc<0 || nc>=N || v[nr][nc]) continue;
+				int gap = Math.abs(arr[p.r][p.c] - arr[nr][nc]);
+				if(gap>=L && gap<=R) {
+					queue.offer(new Point(nr, nc));
+					v[nr][nc] = true;
+					list.add(new Point(nr, nc));
+				}
 			}
 		}
 	}

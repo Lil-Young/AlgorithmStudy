c124641291e66b5fc37e0fd88eb4778456e40c7f
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2025-11-11 22:50:06 +0900

    [Silver III] Title: N과 M (1), Time: 1648 ms, Memory: 56640 KB -BaekjoonHub

--- Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,32 +1,33 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 	static int N, M;
 	static int[] arr;
 	static int[] sel;
 	static boolean[] v;
-
+	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		// 1~N개 배열 만들기
 		arr = new int[N];
 		for(int i=1; i<N+1; i++) {
 			arr[i-1] = i;
-		}		
+		}
 		sel = new int[M];
-		v = new boolean[arr.length];		
-		// 순열
+		v = new boolean[N];
+		
 		permutation(0);
 	}
+	
 	private static void permutation(int k) {
 		if(sel.length == k) {
-			for(int i=0; i<sel.length; i++) {
-				System.out.print(sel[i]+" ");
+//			System.out.println(Arrays.toString(sel));
+			for(int val : sel) {
+				System.out.print(val + " ");
 			}
 			System.out.println();
 			return;
@@ -40,7 +41,5 @@ public class Main {
 				v[i] = false;
 			}
 		}
-		
-		
 	}
 }

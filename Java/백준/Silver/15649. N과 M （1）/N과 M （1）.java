c7182cd853bd5f1eb47afe553fdc390981c48a45
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2025-11-11 23:02:30 +0900

    [Silver III] Title: N과 M (1), Time: 284 ms, Memory: 35472 KB -BaekjoonHub

--- Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,43 +1,44 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	static int N, M;
 	static int[] arr;
 	static int[] sel;
 	static boolean[] v;
-	
-	public static void main(String[] args) throws Exception{
+	static StringBuilder sb;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+		sb = new StringBuilder();
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
 		arr = new int[N];
-		for(int i=1; i<N+1; i++) {
+		for (int i = 1; i < N+1; i++) {
 			arr[i-1] = i;
 		}
 		sel = new int[M];
 		v = new boolean[N];
-		
+		// 중복 없이 M개를 고른 수열
+		// -> 순서가 있으므로 순열
 		permutation(0);
+		System.out.println(sb);
 	}
-	
-	private static void permutation(int k) {
-		if(sel.length == k) {
-//			System.out.println(Arrays.toString(sel));
-			for(int val : sel) {
-				System.out.print(val + " ");
+	private static void permutation(int idx) {
+		if(idx == M) {
+			for (int val : sel) {
+				sb.append(val + " ");
 			}
-			System.out.println();
+			sb.append('\n');
 			return;
 		}
 		
-		for(int i=0; i<arr.length; i++) {
+		
+		for(int i=0; i<N; i++) {
 			if(!v[i]) {
 				v[i] = true;
-				sel[k] = arr[i];
-				permutation(k+1);
+				sel[idx] = arr[i];
+				permutation(idx+1);
 				v[i] = false;
 			}
 		}

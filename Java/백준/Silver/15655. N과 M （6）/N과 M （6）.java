4445471519103597cfa2b515949aab0558a41e33
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2025-11-16 23:04:27 +0900

    [Silver III] Title: N과 M (6), Time: 116 ms, Memory: 15768 KB -BaekjoonHub

--- Java/백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ Java/백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -2,38 +2,37 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	// 걍 입력값을 받는 조합이다.
 	static int N, M;
 	static int[] arr;
 	static int[] sel;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws Exception{
+	static StringBuilder sb;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine().trim());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new int[N];
 		st = new StringTokenizer(br.readLine().trim());
-		for(int i=0; i<N; i++) {
+		for(int i=0; i<arr.length; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(arr);
 		sel = new int[M];
 		
+		
+		Arrays.sort(arr);
+		sb = new StringBuilder();
 		combination(0, 0);
 		System.out.println(sb);
 	}
 	private static void combination(int idx, int k) {
-		if(sel.length == k) {
+		if(k == sel.length) {
 			for(int val : sel) {
-				sb.append(val).append(" ");
+				sb.append(val + " ");
 			}
 			sb.append('\n');
 			return;
 		}
-		if(arr.length == idx) return;
-		
+		if(idx == arr.length) return;
 		
 		sel[k] = arr[idx];
 		combination(idx+1, k+1);

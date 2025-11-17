7a7eaf76ef8a17f64ca5c076a47c41704667f4a1
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2025-11-17 23:26:49 +0900

    [Silver III] Title: N과 M (7), Time: 708 ms, Memory: 265164 KB -BaekjoonHub

--- Java/백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ Java/백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -2,42 +2,41 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	//걍 중복순열이다.
 	static int N, M;
 	static int[] arr;
 	static int[] sel;
-	static boolean[] v;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws Exception{
+	static StringBuilder sb;
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine().trim());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new int[N];
 		st = new StringTokenizer(br.readLine().trim());
-		for(int i=0; i<N; i++) {
+		for(int i=0; i<arr.length; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(arr);
 		sel = new int[M];
-		v = new boolean[N];
 		
-		repeat_permutation(0);
+		
+		Arrays.sort(arr);
+		sb = new StringBuilder();
+		repeatPermutation(0);
 		System.out.println(sb);
 	}
-	private static void repeat_permutation(int k) {
-		if(sel.length == k) {
+	private static void repeatPermutation(int idx) {
+		if(idx == sel.length) {
+//			System.out.println(Arrays.toString(sel));
 			for(int val : sel) {
-				sb.append(val).append(" ");
+				sb.append(val + " ");
 			}
-			sb.append("\n");
+			sb.append('\n');
 			return;
 		}
 		
-		for(int i=0; i<arr.length; i++) {
-			sel[k] = arr[i];
-			repeat_permutation(k+1);
+		for (int i = 0; i < N; i++) {
+			sel[idx] = arr[i];
+			repeatPermutation(idx+1);
 		}
 	}
 }

84451bc636a76bdf31ddf41e31c3c544aac76eed
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2025-11-19 23:36:43 +0900

    [Gold IV] Title: 빙산, Time: 404 ms, Memory: 50104 KB -BaekjoonHub

--- Java/백준/Gold/2573. 빙산/빙산.java
+++ Java/백준/Gold/2573. 빙산/빙산.java
@@ -1,140 +1,109 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-/*
- * 빙산은 양의정수, 바다는 0
- * 각 빙산은 4방향에 0이 저장된 개수만큼 줄어든다.
- * 단, 각 칸에 저장된 높이는 0보다 더 줄어들지 않는다.
- * 한 덩어리 빙산이 주어질 때, 이 빙산이 두 덩어리 이상으로 분리되는 "최초의 시간(년)"을 구해라
- * 만일, 전부 다 녹을 때까지 두 덩어리 이상으로 분리되지 않으면 0을 출력한다.
- */
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N, M, result, s_r, s_c;
+	static int N, M;
 	static int[][] arr;
-	static int[][] reduce;
 	static boolean[][] v;
+	static List<Point> iceList;
+	static int year, iceCount;
+	static boolean isDivide;
 	static int[] dr = {0, 0, 1, -1};
 	static int[] dc = {1, -1, 0, 0};
+	static int[][] zeroNumber;
+	
 	
 	static class Point {
-		int r, c;
-		public Point(int r, int c) {
-			this.r=r;
-			this.c=c;
+		int x, y;
+		Point(int x, int y) {
+			this.x=x;
+			this.y=y;
 		}
 	}
-	static void print() {
-		for (int i = 0; i < N; i++) {
-			System.out.println(Arrays.toString(arr[i]));
-		}
-		System.out.println();
-		for (int i = 0; i < N; i++) {
-			System.out.println(Arrays.toString(v[i]));
-		}
-		System.out.println();
-	}
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
+		// 빙산 1, 바다 0
+		// 빙산의 각 부분에 해당되는 칸에 있는 높이는 일년마다 그 칸에 동서남북 네 방향으로 붙어있는 0이 저장된 칸의 개수만큼 줄어든다. (0보다 더 줄어들지는 않는다.)
+		// 한 덩어리의 빙산이 주어질 때, 이 빙산이 두 덩어리 이상으로 분리되는 최초의 시간(년)을 구하는 프로그램을 작성하시오. (다 녹을 때까지 두 덩어리 이상 분리되지 않으면 0 출력)
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine().trim());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new int[N][M];
-		reduce = new int[N][M];
-		List<Point> list = new ArrayList<>();
+		iceList = new ArrayList<Point>();
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine().trim());
 			for (int j = 0; j < M; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
-				if(arr[i][j] > 0) list.add(new Point(i, j));
+				if(arr[i][j] > 0) iceList.add(new Point(i, j));
 			}
 		}
 		
-		result = 0;
-		while(true) {
-			// 4방 0 개수 세기
-			for (int l = 0; l < list.size(); l++) {
-				Point p = list.get(l);
-				int count = zeroCount(p.r, p.c);
-				reduce[p.r][p.c] = count; 
+		year = 0;
+		isDivide = false;
+		zeroNumber = new int[N][M];
+		while(!isDivide && !iceList.isEmpty()) {
+			// 각 빙산 위치마다 0의 개수를 센다
+			for(Point p : iceList) {
+				int cnt = zeroCount(p);
+				zeroNumber[p.x][p.y] = cnt; 
 			}
-			
-			// 각 빙산 녹는다.
-			for (int l = 0; l < list.size(); l++) {
-				Point p = list.get(l);
-				int num = arr[p.r][p.c] - reduce[p.r][p.c];
-				if(num <= 0) {
-					num = 0;
-					list.remove(l--);
+
+			// 각 빙산을 녹인다.
+			for(int i=0; i<iceList.size(); i++) {
+				Point p = iceList.get(i);
+				if(arr[p.x][p.y] - zeroNumber[p.x][p.y] <= 0) {
+					arr[p.x][p.y] = 0;
+					iceList.remove(i--);
+				}else {
+					arr[p.x][p.y] -= zeroNumber[p.x][p.y]; 
 				}
-				arr[p.r][p.c] = num;
-				
-				reduce[p.r][p.c] = 0;
+				zeroNumber[p.x][p.y] = 0;
 			}
-			
-			// 빙산 덩어리 개수를 센다.
-			int iceCount = 0;
+
+			// 빙산 덩어리 갯수를 센다.
+			iceCount = 0;
 			v = new boolean[N][M];
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if(arr[i][j] > 0 && !v[i][j]) {
-						dfs(i, j);
-						iceCount++;
-					}
+			for(Point p : iceList) {
+				if(!v[p.x][p.y]) {
+					dfs(p.x, p.y);
+					iceCount++;
 				}
 			}
-//			print();
-//			System.out.println(iceCount);
-			result++;
-			if(iceCount >=2) {
-				break;
-			}
-			if(list.size() == 0) break;
+			
+			if(iceCount >= 2) isDivide = true;
+			year++;
 		}
 		
-		int iceCount2 = 0;
-		v = new boolean[N][M];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if(arr[i][j] > 0 && !v[i][j]) {
-					dfs(i, j);
-					iceCount2++;
-				}
-			}
+		if(iceList.isEmpty()) {
+			System.out.println(0); return;
 		}
-		if(list.size() == 0) {
-			System.out.println(0);
-		}else {
-			System.out.println(result);
-		}
+		System.out.println(year);
+		
 	}
+	
+	private static int zeroCount(Point p) {
+		int num = 0;
+		for(int d=0; d<4; d++) {
+			int nr = p.x + dr[d];
+			int nc = p.y + dc[d];
+			if(nr<0 || nr>=N || nc<0 || nc>=M) continue;
+			if(arr[nr][nc] == 0) num++;
+		}
+		
+		return num;
+	}
+	
 	private static void dfs(int r, int c) {
-		// TODO Auto-generated method stub
 		v[r][c] = true;
 		for (int d = 0; d < 4; d++) {
 			int nr = r + dr[d];
 			int nc = c + dc[d];
-			if(nr>=0 && nr<N && nc>=0 && nc<M && arr[nr][nc] > 0 && !v[nr][nc]) {
-				dfs(nr, nc);
-			}
+			if(nr<0 || nr>=N || nc<0 || nc>=M || v[nr][nc] || arr[nr][nc] <= 0) continue;
+			dfs(nr, nc);
 		}
 	}
-	private static int zeroCount(int r, int c) {
-		// TODO Auto-generated method stub
-		int count = 0;
-		for (int d = 0; d < 4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			if(nr>=0 && nr<N && nc>=0 && nc<M && arr[nr][nc] == 0) {
-				count++;
-			}
-		}
-		return count;
-	}
+	
 }
+
+
+
